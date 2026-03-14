4f065cbdcd2c37b33a2f71d67e220787a738d95b
CV-EPIC <pratamainsanc@gmail.com>
2026-03-14 13:29:59 +0800

    fix function

--- js/import.js
+++ js/import.js
@@ -1,31 +1,27 @@
 async function importCSV(){
 
-const file = document.getElementById("csvFile").files[0]
-
-const reader = new FileReader()
-
 reader.onload = async function(e){
 
-const text = e.target.result
-
-const rows = text.split("\n").slice(1)
+const rows = e.target.result.split("\n")
 
 const data = rows.map(r=>{
-
 const c = r.split(",")
 
 return{
 tanggal:c[0],
 no_po:c[1],
 outlet:c[2],
-qty:parseInt(c[4]),
-sku:c[5]
+sku:c[3],
+qty:parseInt(c[4])
 }
 
 })
 
 await fetch("/.netlify/functions/importPenjualan",{
 method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
 body:JSON.stringify(data)
 })
 
